42b17954a8a66f1374bfc7a5ab1f12d467fc9b78
ssaenz1030 <ssaenz@newtech.coppellisd.com>
2021-09-02 15:22:58 -0500

    Update Notes01_Printing_and_Escape_Sequences.java

--- src/notes/Notes01_Printing_and_Escape_Sequences.java
+++ src/notes/Notes01_Printing_and_Escape_Sequences.java
@@ -4,8 +4,9 @@ public class Notes01_Printing_and_Escape_Sequences {
 
 	public static void main(String[] args) {
 		
-		
-		
+	System.out.print("hey!!!");	
+	System.out.println("I am great how about U");
+	System.out.println("Beetlejuice said, \"say my name\"");
 	}
 	
 	
